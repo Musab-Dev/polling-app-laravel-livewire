9edc87a5e46a46e0cfa54e12f28b2159c7de4fe5
Musab-Dev <musab.dev98@gmail.com>
2024-01-16 14:11:51 +0300

    [front-end] add poll options

--- app/Livewire/CreatePoll.php
+++ app/Livewire/CreatePoll.php
@@ -7,7 +7,17 @@ use Livewire\Component;
 class CreatePoll extends Component
 {
     public $title;
+    public $options = [];
 
+    public function addOption() {
+        $this->options[] = '';
+    }
+
+    public function removeOption(int $index){
+        unset($this->options[$index]);
+        // to remove the empty elements and re-index the array
+        $this->options = array_values($this->options);
+    }
     public function render()
     {
         return view('livewire.create-poll');
--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -1,7 +1,24 @@
 <div>
+    <form>
+        <label for="title">Poll Title</label>
+        <input type="text" id="title" wire:model.live="title" placeholder="type the poll title here..." required />
+        <hr class="my-3" />
+        <p>title: {{ $title }}</p>
 
-    <label for="title">Poll Title</label>
-    <input type="text" id="title" wire:model.live="title" placeholder="type the poll title here..." required />
+        <div>
+            <button class="btn" wire:click.prevent="addOption">Add Option</button>
+        </div>
 
-    <p>title: {{ $title }}</p>
+        <div>
+            @foreach ($options as $index => $option)
+                <div class="my-2">
+                    <label>Option {{ $index + 1 }}</label>
+                    <div class="flex gap-2">
+                        <input type="text" wire:model="options.{{ $index }}" />
+                        <button class="btn" wire:click.prevent="removeOption({{ $index }})">Remove</button>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </form>
 </div>
